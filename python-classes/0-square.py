c3c12c58f546966ff625ec1043dc6a8354b722ca
Ahmed <87006304+edtghff@users.noreply.github.com>
2025-11-22 23:49:41 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size):
-        self.__size = size
+"""This module defines a Square class."""
 
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initializes the square with a specific size."""
+        self._Square__size = size
+
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
